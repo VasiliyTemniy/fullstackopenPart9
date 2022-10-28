c1273923ded535ed111694314a2bcc664a598d7a
VasiliyTemniy <flash_er@mail.ru>
2022-10-28 15:54:00 +0300

    9.10 - ready!

--- patientor-backend/index.ts
+++ patientor-backend/index.ts
@@ -3,18 +3,24 @@ import cors from 'cors';
 const app = express();
 app.use(express.json());
 
+import diagnosesRouter from './routes/diagnoses';
+
 app.use((_req, _res, next) => {
   next();
 }, cors({ maxAge: 84600 }));
 
 //app.use(express.static('build'));
 
-const PORT = 3001;
+
+
+app.use('/api/diagnoses', diagnosesRouter);
 
 app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
