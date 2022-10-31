dbf269fad2de5c00cfaf7c367dca8ed1951bf6d9
VasiliyTemniy <flash_er@mail.ru>
2022-10-31 18:03:03 +0300

    move addNewPatient logic

--- patientor-frontend/src/PatientListPage/index.tsx
+++ patientor-frontend/src/PatientListPage/index.tsx
@@ -1,17 +1,17 @@
 import React from "react";
-import axios from "axios";
 import { Box, Table, Button, TableHead, Typography, Link } from "@material-ui/core";
 import { Link as RouterLink } from "react-router-dom";
 
 import { PatientFormValues } from "../AddPatientModal/AddPatientForm";
 import AddPatientModal from "../AddPatientModal";
 import { Patient } from "../types";
-import { apiBaseUrl } from "../constants";
 import HealthRatingBar from "../components/HealthRatingBar";
 import { useStateValue } from "../state";
 import { TableCell } from "@material-ui/core";
 import { TableRow } from "@material-ui/core";
 import { TableBody } from "@material-ui/core";
+
+import { addNewPatient } from "../state";
 
 const PatientListPage = () => {
   const [{ patients }, dispatch] = useStateValue();
@@ -26,23 +26,9 @@ const PatientListPage = () => {
     setError(undefined);
   };
 
-  const submitNewPatient = async (values: PatientFormValues) => {
-    try {
-      const { data: newPatient } = await axios.post<Patient>(
-        `${apiBaseUrl}/patients`,
-        values
-      );
-      dispatch({ type: "ADD_PATIENT", payload: newPatient });
-      closeModal();
-    } catch (e: unknown) {
-      if (axios.isAxiosError(e)) {
-        console.error(e?.response?.data || "Unrecognized axios error");
-        setError(String(e?.response?.data?.error) || "Unrecognized axios error");
-      } else {
-        console.error("Unknown error", e);
-        setError("Unknown error");
-      }
-    }
+  const submitNewPatient = (values: PatientFormValues) => {
+    void addNewPatient(dispatch, { ...values }, setError);
+    closeModal();
   };
 
   return (
