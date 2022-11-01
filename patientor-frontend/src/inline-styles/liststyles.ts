16ecf1514cc264fa9ab1fb91d465db6c3d3e88ed
VasiliyTemniy <flash_er@mail.ru>
2022-11-01 22:47:49 +0300

    add inline styles

---
+++ patientor-frontend/src/inline-styles/liststyles.ts
@@ -0,0 +1,22 @@
+export const listItemStyleNoHighlight = {
+  border: "thick double #BDBDBD",
+  borderRadius: "0.5rem",
+  margin: "0.5rem 0",
+};
+
+export const listItemStyleHighlight = {
+  border: "thick double #BDBDBD",
+  borderRadius: "0.5rem",
+  margin: "0.5rem 0",
+  background: "white",
+};
+
+export const diagnosesBoxStyle = {
+  margin: "0.5rem 0",
+  borderLeft: "2px solid #BDBDBD",
+  width: "100%",
+};
+
+export const textLine = {
+  marginTop: "0.3rem",
+};
